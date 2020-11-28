72a5e7cf38367a1474f74d3c13dc141e598f2ada
jaguile6 <75176528+jaguile6@users.noreply.github.com>
2020-11-28 23:39:07 +0100

    Create FlappyBirduino.ino

---
+++ src/FlappyBirduino.ino
@@ -0,0 +1,3 @@
+#include <SPI.h> //imports the SPI library (needed to communicate with Gamebuino's screen)
+#include <Gamebuino.h> //imports the Gamebuino library
+Gamebuino gb; //creates a Gamebuino object named gb
